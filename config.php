5f6a93d3e0a6da7de8bf5ce84d30ae58720f0108
Brandon Patterson <brandonnpatterson@gmail.com>
2017-05-23 13:32:42 -0500

    transitioning to PHP-Mailer

--- config.php
+++ config.php
@@ -1,20 +1,21 @@
 <?php
-//Helper functions. Likely place in external file
 function _e($string) {
 	return htmlentities($string, ENT_QUOTES, 'UTF-8', false);
 }
-
-$headers = "From: brandnpatterson@gmail.com\r\n";
+// headers
+$headers = "From: smtp.gmail.com\r\n";
 $headers .= "Reply-To: brandnpatterson@gmail.com\r\n";
 $headers .= "CC: brandnpatterson@gmail.com\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
+// template
 $template = [
 	'subject' => 'Reciept for Lessons',
 	'whitelist' => ['name', 'email', 'starting_date', 'ending_date']
 ];
+// create errors array
 $errors = $fields = [];
+// check for errors
 if (!empty($_POST)) {
 	if (!empty($_POST['email']) && ! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 		$errors[] = 'That is not a valid email address';
@@ -27,19 +28,20 @@ if (!empty($_POST)) {
 			$errors[] = 'Please enter your ' . $key;
 		}
 	}
+	// create variables to use in message
 	$starting_date = $fields['starting_date'];
 	$ending_date = $fields['ending_date'];
-
 	$send_to = $fields['name'];
-	$template['message'] = "
-	<h3>Thanks for your payment, $send_to!</h3>
-	<h4>You've paid for lessons for the following dates:</h4>
-	<h4>Starting: $starting_date</h4>
-	<h4>Ending: $ending_date</h4>
-	<p>Brandon Patterson</p>
-	<p>(979) 236-4789</p>
-	";
 	$template['email'] = $fields['email'];
+	// message to be sent
+	$template['message'] = "
+		<h3>Thanks for your payment, $send_to!</h3>
+		<h4>You've paid for lessons for the following dates:</h4>
+		<h4>Starting: $starting_date</h4>
+		<h4>Ending: $ending_date</h4>
+		<p>Brandon Patterson</p>
+		<p>(979) 236-4789</p>
+	";
 	if (empty($errors)) {
 		$sent = mail($template['email'], $template['subject'], $template['message'], $headers);
 	}
--- index.php
+++ index.php
@@ -5,13 +5,15 @@
 		<meta charset="utf-8">
 		<title>Lessons Reciept Form</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<link rel="stylesheet" href="index.css" />
+		<link rel="stylesheet" href="css/index.css" />
 	</head>
 	<body>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h1>Enter your message below</h1>
+					<h2>Lessons Reciept Form</h2>
+					<hr />
+					<h3>Transaction Info</h3>
 					<?php if (!empty($errors)) : ?>
 					<div class="errors">
 						<p class="bg-danger">
@@ -25,10 +27,10 @@
 					<?php endif; ?>
 					<form role="form" method="post" action="index.php">
 						<div class="form-group">
-							<input type="text" class="form-control" name="name" placeholder="Name" value="">
+							<input type="text" class="form-control" name="name" placeholder="Client Name" value="">
 						</div>
 						<div class="form-group">
-							<input type="email" class="form-control" name="email" placeholder="E-mail" value="">
+							<input type="email" class="form-control" name="email" placeholder="Client E-mail" value="">
 						</div>
 						<div class="form-group">
 							<input type="date" class="form-control" name="starting_date"></input>
